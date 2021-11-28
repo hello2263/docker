b4eb9ed744af904e087966cc85f9c24f12e781c4
root <hello2263@naver.com>
2021-11-28 10:36:01 +0900

    mongodb error fix

--- flask/app.py
+++ flask/app.py
@@ -6,7 +6,7 @@ from pymongo.cursor import CursorType
 import requests, json, time, sys, os
 
 app = Flask(__name__)
-host = "16.165.203.102"
+host = "172.17.0.2"
 port = "27017"
 mongo = MongoClient(host, int(port), connect=False)
 mydb = mongo['alarm']
@@ -17,8 +17,7 @@ def update_item_one(mongo, condition=None, update_value=None, db_name=None, coll
     return result
 
 def find_item(mongo, condition=None, db_name=None, collection_name=None):
-    # result = mongo[db_name][collection_name].find(condition, {"_id":False}, no_cursor_timeout=True, cursor_type=CursorType.EXHAUST)
-    result = mongo[db_name][collection_name].find(condition, {"_id":False}, cursor_type=CursorType.EXHAUST)
+    result = mongo[db_name][collection_name].find(condition, {"_id":False})
     return result
 
 @app.route('/')
@@ -38,7 +37,6 @@ def kakao_check_owner():
         kakao_owner_token()
         user = kakao_owner_check()
         kakao_friends_update()
-        # kakao_friends_read()
         return render_template('kakao_owner.html', user = user)
     else:
         return render_template('kakao_owner.html')
@@ -55,18 +53,12 @@ def kakao_friends():
     else:
         return render_template('kakao_friends.html')
     
-
 @app.route('/weather', methods=['GET', 'POST'])
 def weather_alarm():
-    # global select_date, send_date, flag_weather, select_flag
-    # select_flag = 0
-    # ctime, count = count_time()
-    # select_date = weather_db.nowtime()
-    # weather = now_weather(select_date)
-    # select_date = nowtime()
-    # weather = set_data_for_weather('20211123-0600')
-    print(set_data_for_weather('20211123-0600'))
-    # return render_template('weather.html')
+    global select_date
+    select_date = nowtime()
+    weather = set_data_for_weather(select_date)
+    return render_template('weather.html', data = weather)
 
 def kakao_owner_check():
     with open("kakao_code_friends_owner.json","r") as fp:
@@ -149,20 +141,20 @@ def kakao_friends_update():
     headers={"Authorization" : "Bearer " + tokens["access_token"]}
     result = json.loads(requests.get(friend_url, headers=headers).text)
     friends_list = result.get("elements")
-    print(friends_list)
-    # try:
-    for friend in friends_list:
-        update_item_one(mongo, {"uuid":str(friend['uuid'])}, {"$set": {"id":str(friend['id']), "name":str(friend['profile_nickname']), "image":str(friend['profile_thumbnail_image'])}}, "alarm", "kakao")
-        print(friend['profile_nickname']+"success")
-    # except:
-    #     print('friends_update fail')
+    try:
+        for friend in friends_list:
+            update_item_one(mongo, {"uuid":str(friend['uuid'])}, {"$set": {"id":str(friend['id']), "name":str(friend['profile_nickname']), "image":str(friend['profile_thumbnail_image'])}}, "alarm", "kakao")
+            print(friend['profile_nickname']+"success")
+    except:
+        print('friends_update fail')
 
 def find_local_from_db():
     cursor = find_item(mongo, None, "alarm", "local")
     for list in cursor:
-       local_name.append(list["city"])
-       local_x.append(list["x"])
-       local_y.append(list["y"])
+        print(list)
+        local_name.append(list["city"])
+        local_x.append(list["x"])
+        local_y.append(list["y"])
     return local_name, local_x, local_y
 
 def nowtime():
@@ -176,6 +168,7 @@ def nowtime():
     else:
         today_day = str(now.day)
     if now.hour < 10:
+        print(now.hour)
         today_hour = '0'+str(now.hour)
     else:
         today_hour = str(now.hour)
@@ -186,12 +179,11 @@ def nowtime():
 def set_data_for_weather(time):
     count = 0
     weather = [{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}]
-    local, x, y = find_local_from_db()
-    data = find_item(mongo, {"date":"20211123-0600"}, "alarm", "weather")
-    return data
-    # for i in cursor:
-    #     weather[count] = i
-    #     count += 1
-    # return weather
+    dict_data = find_item(mongo, {"date":time}, "alarm", "weather")
+    for i in dict_data:
+        print(i)
+        weather[count] = i
+        count += 1
+    return weather
 
 
