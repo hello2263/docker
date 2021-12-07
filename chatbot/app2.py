7afedaa75c1fca1d70cb2dc54ad22b6eb4e45452
root <hello2263@naver.com>
2021-12-07 21:41:15 +0900

    today finish

--- chatbot/app2.py
+++ chatbot/app2.py
@@ -4,8 +4,8 @@ from datetime import datetime
 from pymongo import MongoClient
 from pymongo.cursor import CursorType
 import requests, json, time, sys, os
-sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname('__file__')))+"/flask/")
-import app1
+# sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname('__file__')))+"/flask/")
+# import app1
 global friend, code
 
 app = Flask(__name__)
@@ -17,6 +17,11 @@ myweather = mydb['weather']
 mysetting = mydb['setting']
 friend = {}
 
+def find_item(mongo, condition=None, db_name=None, collection_name=None):
+    result = mongo[db_name][collection_name].find(condition, {"_id":False})
+    return result
+
+
 def update_item_one(mongo, condition=None, update_value=None, db_name=None, collection_name=None):
     result = mongo[db_name][collection_name].update_one(filter=condition, update=update_value, upsert=True)
     return result
@@ -26,7 +31,7 @@ def home():
     return "Hello, Flask"
 
 def alarm_setting_update():
-    update_item_one(mongo, {"name":str(friend['name'])}, {"$set": {"local":str(friend['local']), "day":str(friend['day']), "time":str(friend['time'])}}, "alarm", "setting")
+    update_item_one(mongo, {"name":str(friend['name'])}, {"$set": {"local":str(friend['local']), "day":str(friend['day']), "time":str(friend['time']), "content":str(friend['content'])}}, "alarm", "setting")
     print("setting update")
 
 @app.route('/experience', methods=['POST'])
@@ -84,21 +89,54 @@ def get_code():
                         ]
             }
         }
-        app1.kakao_to_friends_get_friendstokens(code)
-        app1.kakao_friends_token()
-        app1.kakao_friends_check()
+        # app1.kakao_to_friends_get_friendstokens(code)
+        # app1.kakao_friends_token()
+        # app1.kakao_friends_check()
+        kakao_to_friends_get_friendstokens(code)
+        kakao_friends_token()
+        kakao_friends_check()
         return jsonify(dataSend)
 
 @app.route('/name', methods=['POST'])
 def get_name():
     global user_name
+    value = {}
     if request.method == 'POST':
         content = request.get_json()
         content = content['action']['params']['kakao_name']
         friend['name'] = content
         print(content)
-        try:
-            user_data = app1.find_item(mongo, {"name":friend['name']}, "alarm", "setting")
+        # user_data = app1.find_item(mongo, {"name":friend['name']}, "alarm", "setting")
+        user_data = find_item(mongo, {"name":friend['name']}, "alarm", "setting")
+        for i in user_data:
+            value = i
+        if value.get('name') == None:
+            dataSend = {
+                "version": "2.0",
+                "template": {
+                    "outputs": [
+                            {
+                                "basicCard": 
+                                    {
+                                        "title":"설정된 알람이 없습니다. 무엇을 하고 싶나요?",
+                                        "buttons": [{
+                                            "action":"message",
+                                            "label":"알람 생성/수정",
+                                            "messageText": "알람 생성/수정"
+                                    },
+                                    {
+                                            "action":"message",
+                                            "label":"알람 삭제",
+                                            "messageText": "알람 삭제"
+                                    }]
+                                    }
+                            }
+                            ]
+                }
+            }
+            return jsonify(dataSend)
+            
+        else:
             user_name = friend['name']
             dataSend = {
                 "version": "2.0",
@@ -124,32 +162,7 @@ def get_name():
                 }
             }
             return jsonify(dataSend)
-        except:
-            dataSend = {
-                "version": "2.0",
-                "template": {
-                    "outputs": [
-                            {
-                                "basicCard": 
-                                    {
-                                        "title":"설정된 알람이 없습니다. 무엇을 하고 싶나요?",
-                                        "buttons": [{
-                                            "action":"message",
-                                            "label":"알람 생성/수정",
-                                            "messageText": "알람 생성/수정"
-                                    },
-                                    {
-                                            "action":"message",
-                                            "label":"알람 삭제",
-                                            "messageText": "알람 삭제"
-                                    }]
-                                    }
-                            }
-                            ]
-                }
-            }
-            return jsonify(dataSend)
-
+            
 @app.route('/delete', methods=['POST'])
 def delete_alarm():
     if request.method == 'POST':
@@ -176,6 +189,7 @@ def set_time():
         friend['local'] = content['local']
         friend['day'] = content['day']
         friend['time'] = content['time']
+        friend['content'] = content['content']
         dataSend = {
             "version": "2.0",
             "template": {
@@ -193,3 +207,37 @@ def set_time():
         print(friend)
         alarm_setting_update()
         return jsonify(dataSend)
+
+
+#--------------------------------------------------------------------------------------------
+def kakao_to_friends_get_friendstokens(code):
+    url = 'https://kauth.kakao.com/oauth/token'
+    authorize_code = code
+    data = {
+        'grant_type':'authorization_code',
+        'client_id':'91d3b37e4651a9c3ab0216abfe877a50',
+        'redirect_uri':'https://3.35.252.82/kakao_friend',
+        'code': authorize_code,
+        }
+    response = requests.post(url, data=data)
+    tokens = response.json()
+    print(tokens)
+    with open("kakao_code_friends_friends.json","w") as fp:
+        json.dump(tokens, fp)
+def kakao_friends_token():
+    with open("./kakao_code_friends_friends.json","r") as fp:
+        tokens = json.load(fp)
+    url="https://kapi.kakao.com/v1/user/access_token_info"
+    headers={"Authorization" : "Bearer " + tokens["access_token"]}
+    response = requests.post(url, headers=headers)
+    print(response.text)
+    return response.text
+    
+def kakao_friends_check():
+    with open("kakao_code_friends_friends.json","r") as fp:
+        tokens = json.load(fp)
+    url="https://kapi.kakao.com/v2/user/me"
+    headers={"Authorization" : "Bearer " + tokens["access_token"]}
+    response = requests.post(url, headers=headers)
+    print(response.text)
+    return response.text
